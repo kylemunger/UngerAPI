242566721e7f429785d9ab92ca058e0fbc846fa9
Kyle Unger <kylemunger99@gmail.com>
2023-08-23 13:20:27 -0400

    init

--- UngerApi.Minimal/Program.cs
+++ UngerApi.Minimal/Program.cs
@@ -1,7 +1,47 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/hello/{name}", (string name) => $"Hello {name}!");
+using Grpc.Net.Client;
+using GrpcJobSchedulerClient;
 
-app.Run();
+namespace UngerApi.Minimal;
+
+public class GrpcClient
+{
+    private readonly JobScheduler.JobSchedulerClient _jobClient;
+
+    public GrpcClient(GrpcChannel channel)
+    {
+        _jobClient = new JobScheduler.JobSchedulerClient(channel);
+    }
+
+    public async Task<JobResponse> CreateJobAsync(JobRequest request)
+    {
+        return await _jobClient.ScheduleJobAsync(request);
+    }
+}
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddSingleton(GrpcChannel.ForAddress("http://jobworker-service"));
+        builder.Services.AddSingleton<GrpcClient>();
+
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Hello World!");
+
+        app.MapGet("/hello/{name}", (string name) => $"Hello {name}!");
+
+        app.MapPost("/createjob", async (JobRequest request) =>
+        {
+            var client = app.Services.GetRequiredService<GrpcClient>();
+            var job = await client.CreateJobAsync(request);
+            return Results.Json(job);
+        });
+
+        app.Run();
+
+    }
+}
